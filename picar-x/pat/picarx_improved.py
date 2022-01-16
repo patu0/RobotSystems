6958f6b2c68be067966b1b40c52f764618f634f4
Patrick Uriarte <97patrick@gmail.com>
2022-01-15 22:32:57 -0800

    2.8

--- picar-x/pat/picarx_improved.py
+++ picar-x/pat/picarx_improved.py
@@ -1,6 +1,4 @@
-# yt# from ezblock import Servo,PWM,fileDB,Pin,ADC
-
-import time, atexit
+import time
 try:
     import sys
 
@@ -62,8 +60,6 @@ class Picarx(object):
         for pin in self.motor_speed_pins:
             pin.period(self.PERIOD)
             pin.prescaler(self.PRESCALER)
-
-
 
     def set_motor_speed(self,motor,speed):
         # global cali_speed_value,cali_dir_value
@@ -200,139 +196,7 @@ class Picarx(object):
         self.set_motor_speed(1, 0)
         self.set_motor_speed(2, 0)
 
-
-    def Get_distance(self):
-        timeout=0.01
-        trig = Pin('D8')
-        echo = Pin('D9')
-
-        trig.low()
-        time.sleep(0.01)
-        trig.high()
-        time.sleep(0.000015)
-        trig.low()
-        pulse_end = 0
-        pulse_start = 0
-        timeout_start = time.time()
-        while echo.value()==0:
-            pulse_start = time.time()
-            if pulse_start - timeout_start > timeout:
-                return -1
-        while echo.value()==1:
-            pulse_end = time.time()
-            if pulse_end - timeout_start > timeout:
-                return -2
-        during = pulse_end - pulse_start
-        cm = round(during * 340 / 2 * 100, 2)
-        #print(cm)
-        return cm
-    
     def cleanup(self):
         self.set_motor_speed(1, 0)
         self.set_motor_speed(2, 0)
         self.set_dir_servo_angle(0)
-
-
-
-if __name__ == "__main__":
-    px = Picarx()
-    px.dir_servo_angle_calibration(3)
-    calib_ang = 3
-
-    while True:
-        maneuver_num = input ("What Maneuver would you like the robot to perform? \n 1: Move Forward and Backward \n 2: Parallel Park to the Left \n 3: Parallel Park to the Right \n 4: Three Point Turn \n 5: Exit Selection \n")
-
-        if maneuver_num == "1":
-            print(f"You picked {maneuver_num} | Move Forward and Backward")
-            #  print("Manuever 1: Move Forward then Move Backwards in Straight Lines")
-            px.set_dir_servo_angle(calib_ang)
-            px.forward(10)
-            print("moved forward")
-            time.sleep(1)
-            px.forward(0)
-            time.sleep(1)
-            print("move backwards")
-            px.backward(10)
-            time.sleep(1)
-            px.backward(0)
-            time.sleep(1)
-            px.stop()
-        elif maneuver_num == "2":
-            print(f"You picked {maneuver_num} | Parallel Park Left")
-            # print("Maneuver 2A: Parallel Park Left")
-            px.set_dir_servo_angle(calib_ang-25)
-            time.sleep(0.01)
-            px.backward(2)
-            time.sleep(1)
-            px.set_dir_servo_angle(calib_ang+20)
-            time.sleep(0.01)
-            px.backward(1)
-            time.sleep(1)
-            px.set_dir_servo_angle(3)
-            time.sleep(0.01)
-            px.backward(0)
-            time.sleep(1)
-            px.stop()
-            px.set_dir_servo_angle(calib_ang)
-        elif maneuver_num == "3":
-            print(f"You picked {maneuver_num} | Parallel Park Right")
-            # print("Maneuver 2A: Parallel Park Left")
-            px.set_dir_servo_angle(calib_ang+25)
-            time.sleep(0.01)
-            px.backward(2)
-            time.sleep(1)
-            px.set_dir_servo_angle(calib_ang-20)
-            time.sleep(0.01)
-            px.backward(1)
-            time.sleep(1)
-            px.set_dir_servo_angle(3)
-            time.sleep(0.01)
-            px.backward(0)
-            time.sleep(1)
-            px.stop()
-            px.set_dir_servo_angle(calib_ang)
-        elif maneuver_num == "4":
-            print(f"You picked {maneuver_num} | Three-Point Turn")
-            px.set_dir_servo_angle(calib_ang-40)
-            time.sleep(0.01)
-            px.forward(3)
-            time.sleep(2)
-            px.stop()
-            px.set_dir_servo_angle(calib_ang+40)
-            time.sleep(0.01)
-            px.backward(2)
-            time.sleep(1)
-            px.stop()
-            px.set_dir_servo_angle(calib_ang-20)
-            time.sleep(0.01)
-            px.forward(3)
-            time.sleep(1.2)
-            px.set_dir_servo_angle(calib_ang)
-            time.sleep(0.01)
-            px.forward(3)
-            time.sleep(0.5)
-            px.stop()
-        elif maneuver_num == "5":
-            break
-        else:
-            print(f"You picked {maneuver_num} | Please pick a valid number!!")
-
-    
-    
-
-    # set_dir_servo_angle(0)
-    # time.sleep(1)
-    # self.set_motor_speed(1, 1)
-    # self.set_motor_speed(2, 1)
-    # camera_servo_pin.angle(0)
-# set_camera_servo1_angle(cam_cal_value_1)
-# set_camera_servo2_angle(cam_cal_value_2)
-# set_dir_servo_angle(dir_cal_value)
-
-# if __name__ == "__main__":
-#     try:
-#         # dir_servo_angle_calibration(-10) 
-#         while 1:
-#             test()
-#     finally: 
-#         stop()

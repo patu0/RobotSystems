5e37cc93c89f6fb15ae65a4167f3dfc5ba745a24
Patrick Uriarte <97patrick@gmail.com>
2022-01-31 13:25:31 -0800

    week3 complete

--- picar-x/pat/lane_follow.py
+++ picar-x/pat/lane_follow.py
@@ -6,14 +6,6 @@ from picarx_improved import Picarx
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-# frame = cv2.imread('C:/Users/97pat/Desktop/tape.png')
-# # frame = cv2.imread('/home/pat/Pictures/tape_rpi.png')
-# hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-# cv2.imshow("hsv", hsv)
-# cv2.waitKey(0)
-
-# Courtesy of the David Tian Towards_Data_Science
-# https://towardsdatascience.com/deeppicar-part-4-lane-following-via-opencv-737dd9e47c96
 
 def detect_edges(frame):
     # Courtesy of the David Tian Towards_Data_Science
@@ -55,6 +47,8 @@ def region_of_interest(edges):
     return cropped_edges
 
 def detect_line_segments(cropped_edges):
+    # Courtesy of the David Tian Towards_Data_Science
+    # https://towardsdatascience.com/deeppicar-part-4-lane-following-via-opencv-737dd9e47c96
     # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
     rho = 1  # distance precision in pixel, i.e. 1 pixel
     angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
@@ -115,61 +109,6 @@ def average_slope_intercept(frame, line_segments):
 
     return lane_lines
 
-
-# def average_slope_intercept(frame, line_segments):
-#     # Courtesy of the David Tian Towards_Data_Science
-#     # https://towardsdatascience.com/deeppicar-part-4-lane-following-via-opencv-737dd9e47c96
-#     """
-#     This function combines line segments into one or two lane lines
-#     If all line slopes are < 0: then we only have detected left lane
-#     If all line slopes are > 0: then we only have detected right lane
-#     """
-#     lane_lines = []
-#     if line_segments is None:
-#         logging.info('No line_segment segments detected')
-#         return lane_lines
-
-#     height, width, _ = frame.shape
-#     left_fit = []
-#     right_fit = []
-
-#     boundary = 0
-#     left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
-#     right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
-
-#     for line_segment in line_segments:
-#         for x1, y1, x2, y2 in line_segment:
-#             if x1 == x2:
-#                 logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
-#                 continue
-#             fit = np.polyfit((x1, x2), (y1, y2), 1)
-#             slope = fit[0]
-#             intercept = fit[1]
-
-#             left_fit.append((slope,intercept))
-#     left_fit_average = np.average(left_fit, axis=0)
-#     if len(left_fit) > 0:
-#         lane_lines.append(make_points(frame, left_fit_average))
-
-#     #         if slope < 0:
-#     #             if x1 < left_region_boundary and x2 < left_region_boundary:
-#     #                 left_fit.append((slope, intercept))
-#     #         else:
-#     #             if x1 > right_region_boundary and x2 > right_region_boundary:
-#     #                 right_fit.append((slope, intercept))
-
-#     # left_fit_average = np.average(left_fit, axis=0)
-#     # if len(left_fit) > 0:
-#     #     lane_lines.append(make_points(frame, left_fit_average))
-
-#     # right_fit_average = np.average(right_fit, axis=0)
-#     # if len(right_fit) > 0:
-#     #     lane_lines.append(make_points(frame, right_fit_average))
-
-#     logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
-#     print(lane_lines)
-#     return lane_lines
-
 def make_points(frame, line):
     # Courtesy of the David Tian Towards_Data_Science
     # https://towardsdatascience.com/deeppicar-part-4-lane-following-via-opencv-737dd9e47c96
@@ -208,6 +147,8 @@ def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
 
 
 def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5 ):
+    # Courtesy of the David Tian Towards_Data_Science
+    # https://towardsdatascience.com/deeppicar-part-4-lane-following-via-opencv-737dd9e47c96
     heading_image = np.zeros_like(frame)
     height, width, _ = frame.shape
 
@@ -232,7 +173,6 @@ def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_wid
 
 def picarx_angle_conversion(steering_angle_old):
     steering_angle = steering_angle_old - 90
-
     if steering_angle < 0:
         steering_angle = steering_angle 
     elif steering_angle > 0:
@@ -242,13 +182,10 @@ def picarx_angle_conversion(steering_angle_old):
     return steering_angle
 
 def frame_process(frame):
-    # frame = cv2.imread('C:/Users/97pat/Desktop/lanes.png')
     lane_lines = detect_lane(frame)
     lane_lines_image = display_lines(frame, lane_lines)
     height, width, _ = frame.shape
     cv2.imshow("lane lines", lane_lines_image)
-    # cv2.waitKey(1000)
-    # cv2.destroyAllWindows
     if len(lane_lines) != 0:
         _, _, left_x2, _ = lane_lines[0][0]
         _, _, right_x2, _ = lane_lines[1][0]
@@ -262,37 +199,11 @@ def frame_process(frame):
         print("steering_angle_old:",steering_angle)
         steering_angle=picarx_angle_conversion(steering_angle)
         print("steering_angle_new:", steering_angle)
-        # heading_line_image=display_heading_line(frame,steering_angle)
-        # cv2.imshow(heading_line_image)
         return steering_angle
     else:
         pass
-# if __name__ == "__main__":
-#     frame = cv2.imread('C:/Users/97pat/Desktop/lanes.png')
-#     lane_lines = detect_lane(frame)
-#     lane_lines_image = display_lines(frame, lane_lines)
-#     height, width, _ = frame.shape
-#     cv2.imshow("lane lines", lane_lines_image)
-#     cv2.waitKey(1000)
-#     cv2.destroyAllWindows
-#     _, _, left_x2, _ = lane_lines[0][0]
-#     _, _, right_x2, _ = lane_lines[1][0]
-#     mid = int(width / 2)
-#     x_offset = (left_x2 + right_x2) / 2 - mid
-#     y_offset = int(height / 2)
 
-#     angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-#     angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
-#     steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
-#     print("steering_angle:",steering_angle)
-
-#     heading_line_image=display_heading_line(frame,steering_angle)
-
-#     cv2.imshow("lane lines", heading_line_image)
-#     cv2.waitKey(5000)
-#     cv2.destroyAllWindows
-#init camera
-print("start color detect")
+print("START LANE FOLLOW")
 camera = PiCamera()
 camera.resolution = (640,480)
 camera.framerate = 24
